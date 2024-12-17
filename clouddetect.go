138ec3a3d89e847c209843adca394f59d96debfa
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-12-17 17:11:10 +0530

    feat: implement base detection functionality

--- clouddetect.go
+++ clouddetect.go
@@ -1,6 +1,15 @@
 package clouddetect
 
 import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/nikhil-prabhu/clouddetect/logging"
+	"github.com/nikhil-prabhu/clouddetect/providers/alibaba"
+	"github.com/nikhil-prabhu/clouddetect/providers/aws"
+	"github.com/nikhil-prabhu/clouddetect/providers/azure"
 	"github.com/nikhil-prabhu/clouddetect/types"
 )
 
@@ -22,13 +31,46 @@ type Provider interface {
 	Identify(chan types.ProviderId)
 }
 
+var providers = map[types.ProviderId]Provider{
+	types.Alibaba: &alibaba.Alibaba{},
+	types.Aws:     &aws.Aws{},
+	types.Azure:   &azure.Azure{},
+}
+
 // Detect detects the host's cloud service provider.
 // If a non-zero timeout is specified, it overrides the default timeout duration.
 func Detect(timeout uint64) types.ProviderId {
-	_ = timeout
+	t := timeout
 	if timeout == 0 {
-		_ = DefaultDetectionTimeout
+		t = DefaultDetectionTimeout
 	}
 
-	return types.Unknown
+	ch := make(chan types.ProviderId, 1)
+	wg := sync.WaitGroup{}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(t)*time.Second)
+	defer cancel()
+
+	for name, provider := range providers {
+		wg.Add(1)
+		go func(name types.ProviderId, provider Provider) {
+			logging.Logger.Debug(fmt.Sprintf("Starting detection routine for %s", name))
+			defer wg.Done()
+			provider.Identify(ch)
+		}(name, provider)
+	}
+
+	go func() {
+		wg.Wait()
+		cancel()
+	}()
+
+	select {
+	case result := <-ch:
+		logging.Logger.Info(fmt.Sprintf("Detected cloud service provider: %s", result))
+		return result
+	case <-ctx.Done():
+		logging.Logger.Error(fmt.Sprintf("Detection timed out after %d seconds", t))
+		return types.Unknown
+	}
 }
